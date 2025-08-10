9a945e3f87fabd6bb740adabb1aba5732a1ab14a
actuarysailor <24359398+actuarysailor@users.noreply.github.com>
2025-08-10 04:40:12 -0400

    chore: Testing change to skip logic

--- scripts/skip-in-ci.js
+++ scripts/skip-in-ci.js
@@ -1,5 +1,5 @@
-if (!process.env.CI && !process.env.PRE_COMMIT) {
-  process.exit(1); // run the real script
-} else {
+if (process.env.CI || process.env.PRE_COMMIT) {
   process.exit(0); // skip
+} else {
+  process.exit(1); // run the real script
 }
